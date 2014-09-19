8a3dd77c16dadfc1a926801d27606333d330c2f9
Brandon <justtopla@gmail.com>
2014-09-19 10:35:23 -0700

    asd

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ io.on('connection', function(socket){
   });
 });
 
-var PORT =  process.env.PORT ||3000;
+var PORT =  process.env.port ||3000;
 http.listen(PORT, function(){
   console.log('listening on *:3000');
 });
