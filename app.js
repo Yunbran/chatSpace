9eaf8843f76fb597fcdaea51e72d16f07ceb8320
Brandon <justtopla@gmail.com>
2014-09-19 15:06:59 -0700

    changed to test

--- app.js
+++ app.js
@@ -1,87 +1,79 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-var mongodb = require("mongodb");
-var mongoose = require("mongoose");
-var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
-
-var server = new mongodb.Server("127.0.0.1", 27017, {});
-var client = new mongodb.Db('test', server);
-
-mongoose.connect(connectionString);
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  console.log("acceptable");
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
-var kittySchema = mongoose.Schema({
-    name: String,
-    message: String
-})
+// Routing
+app.use(express.static(__dirname + '/public'));
 
-kittySchema.methods.speak = function () {
-  var greeting = this.name
-    ? "Meow name is " + this.name
-    : "I don't have a name"
-  console.log(greeting);
-}
+// Chatroom
 
-var Kitten = mongoose.model('Kitten', kittySchema)
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
 
+io.on('connection', function (socket) {
+  var addedUser = false;
 
-var silence = new Kitten({ name: 'Silence' })
-console.log(silence.name) // 'Silence'
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
 
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 
-var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak() // "Meow name is fluffy"
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
 
-fluffy.save(function (err, fluffy) {
-  if (err) return console.error(err);
-  fluffy.speak();
-});
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
 
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
 
-  var getData = function(){
-    var result = [];
-
-  Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    var results = [];
-    for(var i=0; i< kittens.length;i++)
-    {
-      results[i] = JSON.stringify(kittens[i].message);
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
     }
-    io.emit('chat message', results);
-});
-
-  return result;};
-
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-
-    getData();
-
-  	socket.on('chat message', function(msg){
-    var messageDoc = new Kitten({ name: 'anonymous', message: msg})
-    messageDoc.save(function (err, message) {
-    if (err) return console.error(err);
-    message.speak();
-  });
-
-    io.emit('chat message', msg);
-
   });
 });
-
-var PORT =  process.env.port ||3000;
-/*http.listen(PORT, function(){
-  console.log('listening on *:3000');
-});*/
-app.listen(PORT);
